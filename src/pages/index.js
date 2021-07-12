ab93531ab540cfed082eef1099a1c9fe07a0f736
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-07-12 17:48:51 -0300

    aula 1

---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import MainGrid from "../components/MainGrid";
+import Box from "../components/Box";
+import { ProfileRelationsBoxWrapper } from "../components/ProfileRelations";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../lib/AluraCommons";
+
+function ProfileSidebar({ githubUser }) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
+
+export default function Home() {
+  const githubUser = "fabriciofs";
+  const pessoasFavoritas = [
+    "juunegreiros",
+    "omariosouto",
+    "peas",
+    "rafaballerini",
+    "marcobrunodev",
+    "vedovelli",
+  ];
+
+  return (
+    <>
+      <AlurakutMenu githubUser={githubUser} />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem Vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => (
+                <li key={itemAtual}>
+                  <a href={`/users/${itemAtual}`}>
+                    <img src={`https://github.com/${itemAtual}.png`} />
+                    <span>{itemAtual}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
+}
